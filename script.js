f8afa496a535c2f5f585b471a434189a0d09944e
lakhdar hafsi <40460234+lacdart2@users.noreply.github.com>
2020-05-24 23:43:13 +0200

    Create  script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Hafsi";
+
+//question 3
+var steps = 8;
+
+// question 4
+var devision = 20 / 5;
+console.log(devision);
+
+// question 5
+var frog;
+console.log(typeof "frog");
+
+// question 6
+var orderHasShipped = true;
+console.log(orderHasShipped);
+
+// question 7 (when saving on vscode it gives an extra parentheses)
+var orderHasShipped = true;
+if ((orderHasShipped = true)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// question 8
+for (i = 0; i <= 9; i++) {
+    console.log(i);
+}
